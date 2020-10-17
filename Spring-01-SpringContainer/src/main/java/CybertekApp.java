6f92c1592e40e198c66a38d48ac26cbcfd3f3888
ssonata74 <ssonata@gmail.com>
2020-10-17 14:49:51 -0400

    Spring Container 1

--- Spring-01-SpringContainer/src/main/java/CybertekApp.java
+++ Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -1,10 +1,21 @@
+import interfaces.Mentor;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class CybertekApp {
     public static void main(String[] args) {
-        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+//        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext container=new ClassPathXmlApplicationContext("config.xml");
 
+        Mentor mentor=(Mentor) container.getBean("fullTimeMentor");
+        mentor.createAccount();
+
+        mentor= (Mentor) container.getBean("partTimeMentor");
+        mentor.createAccount();
+
+        Mentor mentor1=container.getBean("fullTimeMentor", Mentor.class);  //2nd parameter(interface)-return object type
+        mentor1.createAccount();
 
     }
 }

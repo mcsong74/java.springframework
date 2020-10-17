785fcb58660c5078ee77d12ca21b479b15479d13
ssonata74 <ssonata@gmail.com>
2020-10-17 12:25:42 -0400

    First Commits - 2 modules added

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,22 @@
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+
+        FullTimeMentor fullTime=new FullTimeMentor();
+        MentorAccount mentor = new MentorAccount(fullTime);
+
+        mentor.manageAccount();
+
+        PartTimeMentor partTime=new PartTimeMentor();
+        MentorAccount mentor2=new MentorAccount(partTime);
+        mentor2.manageAccount();
+
+        MentorAccount mentor3=new MentorAccount(new FullTimeMentor());
+        mentor3.manageAccount();
+        MentorAccount mentor4= new MentorAccount(new PartTimeMentor());
+        mentor4.manageAccount();
+    }
+}
---
+++ Spring-00-LooselyCoupled/src/service/MentorAccount.java
@@ -0,0 +1,17 @@
+package service;
+
+import implementation.Mentor;
+
+public class MentorAccount {
+    //create compositions from interface
+    Mentor mentor; //Loosely coupled
+
+    //constructor
+    public MentorAccount(Mentor mentor) {
+        this.mentor = mentor;   //polymorphism occurs here
+    }
+
+    public void manageAccount(){
+        this.mentor.createAccount();
+    }
+}

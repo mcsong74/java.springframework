e0ffc796c7931b4e3f1b7c6e65e23f9510e69d3d
ssonata74 <ssonata@gmail.com>
2020-10-20 21:10:58 -0400

    init and destroy method

---
+++ Spring-02-DI-SetterInjection/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,32 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSession;
+
+public class Java implements Course {
+    private ExtraSession extraSession;
+    public void setExtraSession(ExtraSession extraSession) {
+        this.extraSession = extraSession;
+    }
+
+
+
+//    private OfficeHours officeHours;
+//    public Java(){
+//
+//    }
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+//
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+//
+
+
+
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours: "+(20+extraSession.getHours()));
+    }
+}
---
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/CybertekApp.java
@@ -0,0 +1,20 @@
+package com.cybertek;
+
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CybertekApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course1 = container.getBean("java",Course.class);    //singleton
+        Course course2 = container.getBean("java", Course.class);   //singleton
+        //singleton - course 1 and 2 reference same object
+        System.out.println("Pointing to the same object: "+(course1==course2));
+        System.out.println("Memory Location for the course1: "+course1);
+        System.out.println("Memory Location for the course2: "+course2);
+
+    }
+
+}

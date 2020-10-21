ba52f2bc9cbd556c2040b4131c2e8e23e5305267
ssonata74 <ssonata@gmail.com>
2020-10-20 21:21:08 -0400

    init and destroy method

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/interfaces/Course.java
@@ -0,0 +1,4 @@
+package com.cybertek.interfaces;
+
+public interface Course {
+}
--- Spring-04-BeanLifeCycle-InitAndDestroyMethod/src/main/java/com/cybertek/services/Java.java
+++ Spring-04-BeanLifeCycle-InitAndDestroyMethod/src/main/java/com/cybertek/services/Java.java
@@ -7,4 +7,10 @@ public class Java implements Course {
     public void getTeachingHours() {
         System.out.println("Weekly Teaching Hours 20");
     }
+    public void myInitMethod(){
+        System.out.println("Executing init method");
+    }
+    public void myDestroyMethod(){
+        System.out.println("Executing destroy method");
+    }
 }

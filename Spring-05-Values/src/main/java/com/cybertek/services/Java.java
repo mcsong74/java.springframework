4a6f802de2642b6fd44d49971037ada6f1b95117
ssonata74 <ssonata@gmail.com>
2020-10-24 13:48:16 -0400

    Spring - Java Annotaions

---
+++ Spring-05-Values/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,40 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Java implements Course {
+    @Value("JD1")
+    private String batch;
+    @Value ("${instructor}")
+    private String instructor;
+    @Value("${days}")
+    private String[] string;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", string=" + Arrays.toString(string) +
+                '}';
+    }
+
+    //constructor injection using @Qualifier
+    private ExtraSessions extraSessions;
+    public Java(@Qualifier("officeHours") ExtraSessions extraSessions) {
+        //in constructor, autowired not required, if multiple, at least one need it
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly Java Class teaching hours: "+(30+extraSessions.getHours())+" hr");
+    }
+}

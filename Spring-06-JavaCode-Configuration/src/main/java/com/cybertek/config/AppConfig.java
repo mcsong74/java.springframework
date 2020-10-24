02aa20f4df9797f249f0e5231f0547be9a44ed94
ssonata74 <ssonata@gmail.com>
2020-10-24 14:43:22 -0400

    Spring - Java Annotaions - Configuration/Bean

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/config/AppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.cybertek.config;
 
+import com.cybertek.interfaces.ExtraSessions;
 import com.cybertek.services.Java;
+import com.cybertek.services.OfficeHours;
 import com.cybertek.services.Selenium;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,11 +14,20 @@ public class AppConfig {
 
     @Bean
     public Java java(){
-        return new Java();
+//        return new Java(officeHours());
+        return new Java(extraSessions());
     }
     @Bean
     public Selenium selenium(){
         return new Selenium();
+    }
+//    @Bean
+//    public OfficeHours officeHours(){
+//        return new OfficeHours();
+//    }
+    @Bean
+    public ExtraSessions extraSessions(){ //ExtraSessions ex=new OfficeHours(); //more loosely couple
+        return new OfficeHours();
     }
 
 }
--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,12 +1,21 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //@Component //commmented for @Bean
 public class Java implements Course {
+    private ExtraSessions extraSessions;
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     @Override
     public void getTeachingHours() {
         System.out.println("Weekly Java class teaching: 30 hr");
+        System.out.println("Weekly Extra office hour session: "+extraSessions.getHours());
     }
 }

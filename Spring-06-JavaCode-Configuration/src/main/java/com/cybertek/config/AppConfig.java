21d98763d42be0ba01e97e655148e9ddbfff43d7
ssonata74 <ssonata@gmail.com>
2020-10-27 19:38:22 -0400

    Spring - Post Construct and Pre destroy

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/config/AppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/config/AppConfig.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class AppConfig {
 
     @Bean
+//    @Scope("prototype")
     public Java java(){ //3rd party class, can inject this way
 //        return new Java(officeHours());
         return new Java(extraSessions());

fabcd91748965fc9496cacabc2ba8b60f182743c
ssonata74 <ssonata@gmail.com>
2020-10-31 14:09:06 -0400

    Spring Boot - Lab - Carpet Calculator

--- Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/interfaces/carpetPrice/Carpet.java
+++ Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/interfaces/carpetPrice/Carpet.java
@@ -1,5 +1,11 @@
 package com.cybertek.interfaces.carpetPrice;
 
+import com.cybertek.enums.City;
+
+import java.math.BigDecimal;
+
 public interface Carpet {
+//    double getSqFtPrice(City city);
+    BigDecimal getSqFtPrice(City city);
 
 }
---
+++ Spring-07-SpringBoot-IoC-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetVA.java
@@ -0,0 +1,13 @@
+package com.cybertek.services.carpet;
+
+import com.cybertek.enums.City;
+import com.cybertek.interfaces.carpetPrice.Carpet;
+
+import java.math.BigDecimal;
+
+public class CarpetVA implements Carpet {
+    @Override
+    public BigDecimal getSqFtPrice(City city) {
+        return null;
+    }
+}

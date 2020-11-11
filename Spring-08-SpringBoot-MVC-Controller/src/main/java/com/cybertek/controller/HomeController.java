20db6d913b832347c2008531a79e7928d364b6d7
ssonata74 <ssonata@gmail.com>
2020-11-10 21:04:57 -0500

    Spring Boot - MVC - controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,10 +1,7 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -32,6 +29,28 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/home/{name}")
+    public String pathVariableEx(@PathVariable("name") String name){
+        System.out.println("name = " + name);
+        return "home";
+    }
+    @GetMapping("/home/{name}/{email}")
+    public String pathVariableEx(@PathVariable("name") String name, @PathVariable("email") String email){
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+        return "home";
+    }
 
+    @GetMapping("/home/course")
+    public String requestParamEx(@RequestParam("course") String course){
+        System.out.println("course = " + course);
+        return "ozzy";
+    }
+
+    @GetMapping(value="/course2")
+    public String requestParamEx2(@RequestParam(value="course2", required=false, defaultValue="Cybertek") String course2){
+        System.out.println("course2 = " + course2);
+        return "ozzy";
+    }
 
 }

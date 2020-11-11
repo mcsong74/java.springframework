0854dfe5ff342480e33bddbf15c927aabb7fcd2c
ssonata74 <ssonata@gmail.com>
2020-11-10 20:21:25 -0500

    Spring Boot - MVC - controller

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,9 +1,36 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping("/") //default "/"
+    public String getRequestMapping1(){
+        //method name is not very important. looking for RequestMapping parameter
+        return "home";
+    }
+    @RequestMapping(method = RequestMethod.GET, value="/ozzy") //most of time using GET or POST
+    public String getRequestMapping2(){
+        return "ozzy";
+    }
+    @RequestMapping(method=RequestMethod.GET, value="/cybertek") //default "/"
+    public String getRequestMapping3(){
+        return "cybertek";
+    }
+
+    @GetMapping("/spring")
+    public String getMappingEx(){
+        return "home";
+    }
+    @PostMapping("/spring")
+    public String postMappingEx(){
+        return "home";
+    }
 
 
 

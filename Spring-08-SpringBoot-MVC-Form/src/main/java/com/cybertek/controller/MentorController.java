0240aab227f60649ad78269b9aa2c9886dabae66
ssonata74 <ssonata@gmail.com>
2020-11-17 20:12:17 -0500

    Spring Boot - MVC - Thymeleaf - Form

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,27 @@
+package com.cybertek.controller;
+
+import com.cybertek.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/register")
+    public String showForm(Model model){
+
+        model.addAttribute("mentor", new Mentor()); //bind mentor object
+        List<String> batchList= Arrays.asList("Select ...", "B6","B7","B8","B9","B10","B11", "B12");
+        model.addAttribute("batchlist", batchList);
+
+        return "/mentor/mentor-register"; //return to html page 'mentor-register' html file
+    }
+
+
+}

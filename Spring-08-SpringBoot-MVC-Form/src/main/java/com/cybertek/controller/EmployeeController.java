525f167cc531dfa427914d588a17697d431ff31f
ssonata74 <ssonata@gmail.com>
2020-11-18 19:50:32 -0500

    Spring Boot - MVC - Thymeleaf - Form - lab - employee registration

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.cybertek.controller;
 
+import com.cybertek.datagenerator.DataGenerator;
+import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String employeeCreate(){
+    public String employeeCreate(Model model){
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getStateList());
         return "/employee/employee-create";
     }
 }

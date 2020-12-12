c37b0b33d4e971b06e0badac0df74e7eaa9a13a5
ssonata74 <ssonata@gmail.com>
2020-12-12 14:53:26 -0500

    PostgresSQL, JPA ORM - configuration w/ SpringFramework, create tables, annotaions, inheritance, repository

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/ComApplication.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/ComApplication.java
@@ -1,13 +1,32 @@
 package com.cybertek;
 
+import com.cybertek.entity.Car;
+import com.cybertek.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class ComApplication {
+	@Autowired //inject CarRepository
+	CarRepository carRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ComApplication.class, args);
+	}
+
+	@PostConstruct
+	public void dataInit(){
+		Car c1=new Car(2010, "BMW", "M5");
+		Car c2=new Car(2020, "Mercedes Benz", "SLK");
+		Car c3=new Car(2019, "Hyundai", "Sonata");
+		Car c4=new Car(2018, "Toyota", "RAV4");
+		carRepository.save(c1);
+		carRepository.save(c2);
+		carRepository.save(c3);
+		carRepository.save(c4);
 	}
 
 }

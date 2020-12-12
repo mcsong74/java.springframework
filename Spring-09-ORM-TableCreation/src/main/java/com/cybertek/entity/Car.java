384db3a5089b24463124418a5ac16776f97b98b0
ssonata74 <ssonata@gmail.com>
2020-12-12 14:46:29 -0500

    PostgresSQL, JPA ORM - configuration w/ SpringFramework, create tables, annotaions, inheritance, repository

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -1,16 +1,29 @@
 package com.cybertek.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity //relation mapping heppens
 @Table(name="cars")
+@NoArgsConstructor
+//@AllArgsConstructor
+@Getter
+@Setter
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private int year;
     private String make;
     private String model;
 
-
+    public Car(int year, String make, String model) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+    }
 }

85bb1344b32d79ccffe5108da86e547423768040
ssonata74 <ssonata@gmail.com>
2020-12-19 10:35:49 -0500

    PostgresSQL, JPA ORM - ORM, Lab - Cinema app

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Movie.java
@@ -0,0 +1,39 @@
+package com.cybertek.entity;
+
+import com.cybertek.enums.MovieState;
+import com.cybertek.enums.MovieType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Movie extends BaseEntity{
+    private String name;
+
+    @Column(columnDefinition = "DATE") //Jave 8
+    private LocalDate releaseDate;
+
+    private Integer duration;
+
+    @Column(columnDefinition = "text") //sets no limitation in character limit
+    private String summary;
+
+    @Enumerated(EnumType.STRING) //if no annotation it will be ORDINAL
+    private MovieType type;
+    @Enumerated(EnumType.STRING)//if no annotation it will be ORDINAL
+    private MovieState state;
+
+    private BigDecimal price;
+
+
+
+
+
+}

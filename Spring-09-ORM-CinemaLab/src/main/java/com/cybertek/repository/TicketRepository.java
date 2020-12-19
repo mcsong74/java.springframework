dcceb6caacc8b669eba18e00d07eea171be5def1
ssonata74 <ssonata@gmail.com>
2020-12-19 15:00:43 -0500

    PostgresSQL, JPA ORM - ORM, Lab - Cinema app

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/TicketRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/TicketRepository.java
@@ -4,6 +4,7 @@ import com.cybertek.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }

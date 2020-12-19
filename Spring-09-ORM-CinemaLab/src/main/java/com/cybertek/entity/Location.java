6ba846e0579fbc77bb5b47c2443c6a046b54707e
ssonata74 <ssonata@gmail.com>
2020-12-19 11:32:49 -0500

    PostgresSQL, JPA ORM - ORM, Lab - Cinema app

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Location.java
@@ -0,0 +1,26 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Location extends BaseEntity{
+
+    private String name;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String country;
+    private String city;
+    private String state;
+    private Integer postalCode;
+    private String address;
+
+
+}

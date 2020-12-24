12795354327a0ef4f85e809bb6827424514027ee
ssonata74 <ssonata@gmail.com>
2020-12-23 19:52:19 -0500

    PostgresSQL, JPA ORM - ORM, Derived Queries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,6 +47,22 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     //Greater than
     @Query("select e from Employee e where e.salary > ?1")
     List<Employee> getEmployeeBySalaryGreaterThan(int salary);
+    //between
+    @Query("select e from Employee e where e.salary between ?1 and ?2")
+    List<Employee> getEmployeeBySalaryBetween(int salary1, int salary2);
+    //before
+    @Query("select e from Employee e where e.hireDate < ?1")
+    List<Employee> getEmployeeByHireDateBefore(LocalDate date);
+
+    //Null
+    @Query("select e from Employee e where e.email is null ")
+    List<Employee> getEmployeeByEmailIsNull();
+    //not null
+    @Query("select e from Employee e where e.email is not null ")
+    List<Employee> getEmployeeByEmailIsNotNull();
+
+
+
 
 
 

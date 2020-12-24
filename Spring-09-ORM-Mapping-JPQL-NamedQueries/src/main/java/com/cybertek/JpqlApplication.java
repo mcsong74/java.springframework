97f078c314a30285ddc068a5eee70bb8ea369c49
ssonata74 <ssonata@gmail.com>
2020-12-23 19:14:34 -0500

    PostgresSQL, JPA ORM - ORM, Derived Queries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/JpqlApplication.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/JpqlApplication.java
@@ -20,8 +20,15 @@ public class JpqlApplication {
 
 	@PostConstruct
 	public void testEmployee(){
-		System.out.println("employeeRepository.getEmployeeDetail() = " + employeeRepository.getEmployeeDetail());
-		System.out.println("employeeRepository.getEmployeeSalary() = " + employeeRepository.getEmployeeSalary());
+		System.out.println("employeeRepository.getEmployeeDetail() = "
+				+ employeeRepository.getEmployeeDetail());
+		System.out.println("employeeRepository.getEmployeeSalary() = "
+				+ employeeRepository.getEmployeeSalary());
+
+		System.out.println("employeeRepository.getEmployeeByEmail(\"myakovlivf@ucsd.edu\") = "
+				+ employeeRepository.getEmployeeByEmail("myakovlivf@ucsd.edu").get());
+
+
 	}
 
 }
--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
@@ -15,7 +17,12 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Query("select e.salary from Employee e where e.email='dtrail8@tamu.edu'")
     Integer getEmployeeSalary();
 
+    //Single bind parameter
+    @Query("select e from Employee e where e.email=?1")
+    Optional<Employee> getEmployeeByEmail(String email); //optional protects null pointer exception when it is null
 
+    @Query("select e from Employee e where e.email=?1 and e.salary=?2")
+    Employee getEmployeeByEmailAndSalary(String email, Integer salary);
 
 
 

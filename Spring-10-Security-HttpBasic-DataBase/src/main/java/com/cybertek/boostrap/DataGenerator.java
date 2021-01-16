913d92d2edc837807c6399c8b031d80ce3100d06
ssonata74 <ssonata@gmail.com>
2021-01-16 13:46:41 -0500

    Spring boot - security - HttpBase Auth Role + database

--- Spring-10-Security-HttpBasic-DataBase/src/main/java/com/cybertek/boostrap/DataGenerator.java
+++ Spring-10-Security-HttpBasic-DataBase/src/main/java/com/cybertek/boostrap/DataGenerator.java
@@ -3,18 +3,28 @@ package com.cybertek.boostrap;
 import com.cybertek.entity.User;
 import com.cybertek.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Service
+@Component
 public class DataGenerator implements CommandLineRunner {
 
     private UserRepository userRepository;
+    PasswordEncoder passwordEncoder;
 
-    public DataGenerator(UserRepository userRepository) {
+    public DataGenerator(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
     }
 
     @Override
@@ -22,13 +32,15 @@ public class DataGenerator implements CommandLineRunner {
         //Delete all
         userRepository.deleteAll();
         //create users
-        User employee= new User("Ozzy", "ozzy123", "USER", "");
-        User admin = new User("admin", "admin123","ADMIN", "ACCESS_TEST1, ACCESS_TEST2");
-        User manager= new User("manager", "manager123", "MANAGER", "ACCESS_TEST1");
+        User employee= new User("Ozzy", passwordEncoder.encode("ozzy123"), "USER", "");
+        User admin = new User("admin", passwordEncoder.encode("admin123"),"ADMIN", "ACCESS_TEST1, ACCESS_TEST2");
+        User manager= new User("manager", passwordEncoder.encode("manager123"), "MANAGER", "ACCESS_TEST1");
 
         List<User> users = Arrays.asList(employee, admin, manager);
 
         userRepository.saveAll(users);
 
     }
+
+
 }

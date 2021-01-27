f70b14fd12d3f2267e42468bbb240fa7f680ebc4
ssonata74 <ssonata@gmail.com>
2021-01-26 20:04:09 -0500

    Spring boot - REST - ResponseEntity

--- Spring-11-REST-03-ResponseEntity/src/main/java/com/cybertek/controller/ProductController.java
+++ Spring-11-REST-03-ResponseEntity/src/main/java/com/cybertek/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.cybertek.controller;
 
 import com.cybertek.entity.Product;
 import com.cybertek.service.ProductService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,20 +18,38 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
     @GetMapping(value="/{id}")
-    public Product getProduct(@PathVariable("id") long id){
-        return productService.getProduct(id);
+    public ResponseEntity<Product> getProduct(@PathVariable("id") long id){
+
+        return ResponseEntity.ok(productService.getProduct(id));
+//        return productService.getProduct(id);
     }
 
     @GetMapping
-    public List<Product> getProducts(){
-        return productService.getProducts();
+    public ResponseEntity<List<Product>> getProducts(){
+        //adding custom header, way 1 - HttpHeaders
+        HttpHeaders responseHttpHeaders = new HttpHeaders();
+        responseHttpHeaders.set("Version", "v1.0.0.12");
+        responseHttpHeaders.set("Operation", "Get List");
+        //For ResponseEntity, need to pass 3 things: status, header, body
+        //return type need to be changed ResponseEntity<List<Product>>
+        return ResponseEntity.ok()//.ok == status 200
+                .headers(responseHttpHeaders)
+                .body(productService.getProducts());
     }
 
     //create Product - POST
     @PostMapping
-    public List<Product> createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    public ResponseEntity<List<Product>> createProduct(@RequestBody Product product){
+        List<Product> set = productService.createProduct(product);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Version", "Cybertek.v1.0.0.1")
+                .header("Operation", "Create ")
+                .body(set);
+
     }
     //Delete Product -DELETE
     @DeleteMapping(value="/{id}")

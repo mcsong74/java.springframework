48f757e544dbd9776acfe631f1f2e5a7c611e9f7
ssonata74 <ssonata@gmail.com>
2021-01-27 21:23:25 -0500

    Spring boot - REST - Rest Template - practice lab

--- Spring-11-REST-06-LAB/src/main/java/com/cybertek/entity/Class.java
+++ Spring-11-REST-06-LAB/src/main/java/com/cybertek/entity/Class.java
@@ -1,5 +1,6 @@
 package com.cybertek.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,9 +12,21 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Table(name="class")
+@JsonIgnoreProperties(value={"hibernateLazyInitializer"}, ignoreUnknown = true)
+
 public class Class extends BaseEntity{
 
     private String name;
     private Integer year;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="course_id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="teacher_id")
+    private Teacher teacher;
+
+
 
 }

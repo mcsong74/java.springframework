29a9dd7bd5d864cb0f5f8bd17c515f9446ce3a01
ssonata74 <ssonata@gmail.com>
2021-01-30 13:34:22 -0500

    Spring boot - REST - Jackson - Reactive - flux/Mono

--- Spring-11-REST-04-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
+++ Spring-11-REST-04-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
@@ -1,12 +1,42 @@
 package com.cybertek.controller;
 
+import com.cybertek.entity.Genre;
+import com.cybertek.entity.MovieCinema;
+import com.cybertek.repository.GenreRepository;
+import com.cybertek.repository.MovieCinemaRepository;
+import com.cybertek.repository.MovieRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 public class WebFluxController  {
 
     private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
 
+    private MovieRepository movieRepository;
+    private MovieCinemaRepository movieCinemaRepository;
+    private GenreRepository genreRepository;
 
+    public WebFluxController(MovieRepository movieRepository, MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
+        this.movieRepository = movieRepository;
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.genreRepository = genreRepository;
+    }
+
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinema> readAllCinemaFlux(){
+        return Flux.fromIterable(movieCinemaRepository.findAll()); //convert List to Flux structure
+    }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readMovieCinemaById(@PathVariable("id") Long id){
+        //retrieving only one then, Mono
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
 }

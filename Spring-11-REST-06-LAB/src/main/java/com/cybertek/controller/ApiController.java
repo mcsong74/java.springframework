dc7ad377bd5cb1d9fe00a2d4ec02c52b57d34b5a
ssonata74 <ssonata@gmail.com>
2021-01-30 10:01:40 -0500

    Spring boot - REST - Rest Template - practice lab

--- Spring-11-REST-06-LAB/src/main/java/com/cybertek/controller/ApiController.java
+++ Spring-11-REST-06-LAB/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,9 +1,13 @@
 package com.cybertek.controller;
 
 import com.cybertek.entity.Address;
+import com.cybertek.entity.Parent;
 import com.cybertek.entity.Student;
+import com.cybertek.entity.Teacher;
 import com.cybertek.repository.AddressRepository;
+import com.cybertek.repository.ParentRepository;
 import com.cybertek.repository.StudentRepository;
+import com.cybertek.repository.TeacherRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +18,14 @@ public class ApiController {
 
     private AddressRepository addressRepository;
     private StudentRepository studentRepository;
+    private TeacherRepository teacherRepository;
+    private ParentRepository parentRepository;
 
-    public ApiController(AddressRepository addressRepository, StudentRepository studentRepository) {
+    public ApiController(AddressRepository addressRepository, StudentRepository studentRepository, TeacherRepository teacherRepository, ParentRepository parentRepository) {
         this.addressRepository = addressRepository;
         this.studentRepository = studentRepository;
+        this.teacherRepository = teacherRepository;
+        this.parentRepository = parentRepository;
     }
 
     @GetMapping("/address")
@@ -29,5 +37,12 @@ public class ApiController {
     public List<Student> readAllStudents(){
         return studentRepository.findAll();
     }
-    @
+    @GetMapping("/teachers")
+    public List<Teacher> readAllTeachers(){
+        return teacherRepository.findAll();
+    }
+    @GetMapping("/parents")
+    public List<Parent> readAllParents(){
+        return parentRepository.findAll();
+    }
 }

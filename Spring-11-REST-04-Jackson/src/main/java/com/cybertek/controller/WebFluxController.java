b5a15955881fafa626d52316375596b16a82faec
ssonata74 <ssonata@gmail.com>
2021-01-30 14:10:49 -0500

    Spring boot - REST - Jackson - Reactive - Web Client, flux/Mono,

--- Spring-11-REST-04-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
+++ Spring-11-REST-04-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
@@ -5,6 +5,8 @@ import com.cybertek.entity.MovieCinema;
 import com.cybertek.repository.GenreRepository;
 import com.cybertek.repository.MovieCinemaRepository;
 import com.cybertek.repository.MovieRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -38,7 +40,7 @@ public class WebFluxController  {
         return Mono.just(movieCinemaRepository.findById(id).get());
     }
     @GetMapping("/mono-movie-cinema")
-    public Mono<MovieCinema readMovieCinemaByIdRequestParam(@RequestParam("id") Long id){
+    public Mono<MovieCinema> readMovieCinemaByIdRequestParam(@RequestParam("id") Long id){
         return Mono.just(movieCinemaRepository.findById(id).get());
     }
 
@@ -61,6 +63,39 @@ public class WebFluxController  {
     }
 
 //------------------------ Web Client -----------------------------
+
+    @GetMapping("/flux")
+    public Flux<MovieCinema> readWithWebClient(){
+        return webClient
+                .get()
+                .uri("/flux-movie-cinemas")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve().bodyToFlux(MovieCinema.class);
+    }
+
+    @GetMapping("/mono/{id}")
+    public Mono<MovieCinema> readMonoWithWebClient(@PathVariable("id") Long id){
+        return webClient
+                .get()
+                .uri("/mono-movie-cinema/{id}", id)
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
+    }
+
+    @GetMapping("/mono-rp")
+    public Mono<MovieCinema> readMonoWithWebClientRequestParam(@RequestParam("id") Long id){
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/mono-movie-cinema")
+                        .queryParam("id", id)
+                        .build())
+                //uriBuilder for @RequestParam
+                .retrieve()
+                .bodyToMono(MovieCinema.class);
+
+    }
+
 
 
 }

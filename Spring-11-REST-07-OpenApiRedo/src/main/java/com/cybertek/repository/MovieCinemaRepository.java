252f318d829da4cd7da78d4d7c1d29e6b62e65d1
ssonata74 <ssonata@gmail.com>
2021-02-05 20:16:21 -0500

    Spring - REST - OpenApi 3.0 - re-do, review

--- Spring-11-REST-07-OpenApiRedo/src/main/java/com/cybertek/repository/MovieCinemaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cybertek.repository;
-
-import com.cybertek.entity.MovieCinema;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieCinemaRepository extends JpaRepository<MovieCinema, Long> {
-}
---
+++ Spring-11-REST-07-openApiReWork/src/main/java/com/cybertek/entity/Account.java
@@ -0,0 +1,34 @@
+package com.cybertek.entity;
+
+import com.cybertek.enums.UserRole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="account_details")
+public class Account extends BaseEntity{
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+
+    private UserRole role= UserRole.USER;
+
+    private User user;
+
+
+
+
+
+}

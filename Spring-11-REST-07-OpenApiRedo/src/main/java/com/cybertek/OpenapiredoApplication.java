e340105c9a42f6be745b5289ac0c54a13579cf49
ssonata74 <ssonata@gmail.com>
2021-02-05 19:28:12 -0500

    Spring - REST - OpenApi 3.0 - re-do, review

---
+++ Spring-11-REST-07-OpenApiRedo/src/main/java/com/cybertek/OpenapiredoApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiredoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiredoApplication.class, args);
+	}
+
+}
